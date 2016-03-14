a3a16719df6af691c07e9806aa3aa0983a762a8b
MrLeeh <Stefan.St.Lehmann@gmail.com>
2016-03-14 16:05:08 +0100

    start with empty resource list

--- app/static/js/recipe_edit.js
+++ app/static/js/recipe_edit.js
@@ -1,11 +1,51 @@
 /*
 * @Author: Stefan Lehmann
 * @Date:   2016
-* @Last Modified by:   Stefan Lehmann
+* @Last Modified by:   lehmann
 * @Last Modified time: 2016
 */
 
 $(document).ready(function() {
+
+  var div_template =
+    '<div class="entry-div col-sm-12">' +
+      '<div class="form-horizontal">' +
+          '<div class="form-group">' +
+            '<div class="col-sm-6">' +
+              '<input class="form-control">' +
+            '</div>' +
+            '<div class="col-sm-2">' +
+              '<div class="input-group">' +
+                '<input class="form-control">' +
+                '<div class="input-group-addon" style="width:30px"></div>' +
+              '</div>' +
+            '</div>' +
+            '<a class="btn btn-link"><span class="glyphicon glyphicon-trash"></span></a>' +
+          '</div>' +
+        '</div>' +
+      '</div>' +
+    '</div>'
+
+
+  function add_entry(type, nr) {
+    html = $.parseHTML(div_template);
+    $('.' + type + '-body').append(html);
+    $div = $('.' + type + '-body .entry-div').last();
+
+    $div.attr('id', type + '-' + nr);
+
+    // name input
+    $name_input = $div.find('input').eq(0);
+    $name_input.attr('id', type + '-' + nr + '-name_');
+    $name_input.attr('name', type + '-' + nr + '-name_');
+
+    // quantity input
+    $qty_input = $div.find('input').eq(1);
+    $qty_input.attr('id', type + '-' + nr + '-qty');
+    $qty_input.attr('name', type + '-' + nr + '-qty');
+  }
+
+
   // add malt
   $('a.add-malt').click(function(event) {
     var last_div = ($('.malt-div')).last()
@@ -54,19 +94,33 @@ $(document).ready(function() {
   }); //End remove row
 
     // add misc ingredient
-  $('a.add-misc').click(function(event) {
-    console.log('click');
-    var last_div = $('.misc-div').last();
-    new_div = last_div.clone(true, true).appendTo('.misc-body');
-    new_num = parseInt(new_div.attr('id').match(/\d+/)) + 1;
+  // $('a.add-misc').click(function(event) {
+  //   var last_div = $('.misc-div').last();
+  //   new_div = last_div.clone(true, true).appendTo('.misc-body');
+  //   new_num = parseInt(new_div.attr('id').match(/\d+/)) + 1;
 
-    new_div.attr('id', 'misc-' + new_num);
-    new_div.find("input[id*='name_']").attr('id', 'misc-' + new_num + '-name_')
-    new_div.find("input[id*='name_']").attr('name', 'misc-' + new_num + '-name_')
-    new_div.find("input[id*='qty']").attr('id', 'misc-' + new_num + '-qty')
-    new_div.find("input[id*='qty']").attr('name', 'misc-' + new_num + '-qty')
+  //   new_div.attr('id', 'misc-' + new_num);
+  //   new_div.find("input[id*='name_']").attr('id', 'misc-' + new_num + '-name_')
+  //   new_div.find("input[id*='name_']").attr('name', 'misc-' + new_num + '-name_')
+  //   new_div.find("input[id*='qty']").attr('id', 'misc-' + new_num + '-qty')
+  //   new_div.find("input[id*='qty']").attr('name', 'misc-' + new_num + '-qty')
+  //   event.stopPropagation();
+  // })
+
+  $('a.add-misc').click(function(event) {
+    var $entries = $('.misc-body .entry-div');
+    var nr = 0
+    if ($entries.length > 0) {
+      nr = parseInt($entries.last().attr('id').match(/\d+/)) + 1;
+    }
+    add_entry('misc', nr);
+    // new_div.attr('id', "malt-" + new_num);
+    // new_div.find("input[id*='name_']").attr('id', 'malts-' + new_num + '-name_')
+    // new_div.find("input[id*='name_']").attr('name', 'malts-' + new_num + '-name_')
+    // new_div.find("input[id*='qty']").attr('id', 'malts-' + new_num + '-qty')
+    // new_div.find("input[id*='qty']").attr('name', 'malts-' + new_num + '-qty')
     event.stopPropagation();
-  })
+  });
 
   // remove misc ingredient
   $('.del-misc').click(function() {
--- app/views.py
+++ app/views.py
@@ -42,13 +42,13 @@ def edit_recipe(recipe_id):
         abort(404)
 
     if len(recipe.malts) == 0:
-        recipe.malts = [RecipeMalt()]
+        recipe.malts = []
 
     if len(recipe.hops) == 0:
-        recipe.hops = [RecipeHop()]
+        recipe.hops = []
 
     if len(recipe.miscs) == 0:
-        recipe.miscs = [RecipeMisc()]
+        recipe.miscs = []
 
     form = RecipeForm(obj=recipe)
     if form.validate_on_submit():
